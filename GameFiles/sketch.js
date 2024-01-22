52b89484556a16128deea7fffac561425476172f
YunaSan <70344578+TylerLeCmd@users.noreply.github.com>
2024-01-22 08:55:43 -0700

    Update sketch.js

--- GameFiles/sketch.js
+++ GameFiles/sketch.js
@@ -1,33 +1,32 @@
-var bugs = [];
 //Player Vars
 var playerPosX = 100;
 var playerPosY = 0;
 var playerSize = 50;
 var playerJump = 35;
+var playerSpeed = 10;
 
 var xSpeed = 0;
 var ySpeed = 0;
 var gravAcc = 1;
 
-let jumpOn = false;
 let color = 0;
+var ground = [];
+let onGround = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
-  for (var i = 0; i < 50; i++) {
-    bugs.push(new Jitter());
-  }
 }
 
 function draw() {
   game();
   //info
-  text(ySpeed, 200, 200);
-  text(playerPosY, 200, 210);
-  text(gravAcc, 200, 220);
-  text(windowHeight - playerSize / 2, 200, 230);
+  // text(ySpeed, 200, 200);
+  // text(playerPosY, 200, 210);
+  // text(gravAcc, 200, 220);
+  // text(windowHeight - playerSize / 2, 200, 230);
   text(`${key} ${keyCode}`, 10, 40);
+  text (onGround, 10, 50);
 }
 
 function game() {
@@ -36,7 +35,16 @@ function game() {
   //Player Render
   fill(255);
   square(playerPosX, playerPosY, playerSize);
+  movement();
 
+}
+
+function movement() {
+  if (keyIsDown(65)) {
+      playerPosX -= playerSpeed;
+  } else if (keyIsDown(68)) {
+      playerPosX += playerSpeed;
+  } else { }
   ySpeed += gravAcc;
   playerPosY += ySpeed;
   if (playerPosY + playerSize / 2 > height) {
@@ -45,18 +53,9 @@ function game() {
   }
   ySpeed = ySpeed * 0.98;
 
-  if (jumpOn) {
+  if (keyIsDown(32)) {
     if (playerPosY + playerSize / 2 >= height) {
       ySpeed = -playerJump;
     }
-    jumpOn = false;
   }
 }
-
-function keyTyped() {
-  print(key, ' ', keyCode);
-  if (keyCode === 32) {
-    jumpOn = true;
-  }
-}
-
